c7fbbd1aa489e4851f0f976e3414101a6423af02
Lynn <lena.scholz@stud.tu-darmstadt.de>
2016-09-30 08:46:36 +0200

    Buttons Animation

--- core/src/com/bp/Tale.java
+++ core/src/com/bp/Tale.java
@@ -2,11 +2,30 @@ package com.bp;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 
-public class Fox implements ApplicationListener {
+import sun.rmi.runtime.Log;
+
+public class Tale implements ApplicationListener {
+
+    private float width;
+    private float height;
+    private float margin = 70f;
+
     private SpriteBatch batch;
     private TextureAtlas textureAtlas;
     private Animation animation;
@@ -14,6 +33,10 @@ public class Fox implements ApplicationListener {
 
     @Override
     public void create() {
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+
+        // animation
         batch = new SpriteBatch();
         textureAtlas = new TextureAtlas(Gdx.files.internal("tale/atlas_tale.atlas"));
         animation = new Animation(1/15f, textureAtlas.getRegions());
@@ -23,6 +46,7 @@ public class Fox implements ApplicationListener {
     public void dispose() {
         batch.dispose();
         textureAtlas.dispose();
+
     }
 
     @Override
@@ -30,21 +54,19 @@ public class Fox implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Animation
         batch.begin();
-        //sprite.draw(batch);
+//        stage.draw();
         elapsedTime += Gdx.graphics.getDeltaTime();
         batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
         batch.end();
     }
-
     @Override
     public void resize(int width, int height) {
     }
-
     @Override
     public void pause() {
     }
-
     @Override
     public void resume() {
     }
