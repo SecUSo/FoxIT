8b9426f461cfb2dafcb7886a51b50cfb584be823
sleep-yearning <emailfurunity@web.de>
2017-12-17 06:33:39 +0100

    link slide is working and ready

--- android/src/com/foxyourprivacy/f0x1t/slides/LinkSlide.java
+++ android/src/com/foxyourprivacy/f0x1t/slides/LinkSlide.java
@@ -1,6 +1,14 @@
 package com.foxyourprivacy.f0x1t.slides;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.foxyourprivacy.f0x1t.R;
 
 import java.util.regex.Pattern;
 
@@ -11,14 +19,26 @@ import java.util.regex.Pattern;
 
 //TODO implement webview
 public class LinkSlide extends Slide {
+
+    View rootView;
     @Override
     public void fillLayout() {
-
+        WebView webView = rootView.findViewById(R.id.linkWebView);
+        webView.loadUrl(slideInfo);
     }
 
     @Override
     public void setArguments(Bundle arg) {
         super.setArguments(arg);
         slideInfo = slideInfo.replaceFirst(Pattern.quote("[LINK]"), "");
+        slideInfo = slideInfo.replaceAll(";", "");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.layout_slide_link, container, false);
+        fillLayout();
+        return rootView;
     }
 }
